dce6e341a659a74e4ee5153a47b818497626cbe4
Deepak Mishra <d3ep4k@users.noreply.github.com>
2015-09-14 21:22:05 +0530

    added source

---
+++ src/jobschedular/IoBoundTask.java
@@ -0,0 +1,27 @@
+package jobschedular;
+
+import java.util.Scanner;
+
+public class IoBoundTask extends Task {
+
+    String filePath;
+
+    public IoBoundTask(int index, int burst, String fileName) {
+        super(index, burst);
+        this.filePath = fileName;
+    }
+
+    /**
+     * IO bound operations. Reading file and printing to console.
+     */
+    @Override
+    void operation() {
+        try (Scanner sc = new Scanner(Task.class.getResourceAsStream(filePath))) {
+            while (sc.hasNextLine()) {
+                String i = sc.next();
+                System.out.println(i);
+            }
+        }
+    }
+
+}
